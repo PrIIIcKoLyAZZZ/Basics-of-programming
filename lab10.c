1b772a9078e40902431846b4859984b71c169a30
PrIIIcKoLyAZZZ <367898@edu.itmo.ru>
2022-12-21 16:01:10 +0300

    New Lab10

--- lab10.c
+++ lab10.c
@@ -1,3 +1,50 @@
-//
-// Created by Maxim Bovt on 21.12.2022.
-//
+#include <stdio.h>
+
+int summa(int n)
+{
+    if (n<10) return n;
+    return summa(n/10) + n % 10;
+}
+
+void space(char* line, int begin, int end)
+{
+    if (begin == end)
+        return;
+    if (begin == 0 && line[begin] == ' ') {
+        for (int i = begin; i < end; ++i) {
+            line[i] = line[i + 1];
+        }
+        line[end] = ' ';
+    }
+    if (line[begin] == ' ' && line[begin + 1] == ' ')
+    {
+        for (int i = begin; i < end; ++i) {
+            line[i] = line[i + 1];
+        }
+        line[end] = ' ';
+    }
+
+    space(line, begin + 1, end);
+}
+
+int main()
+{
+    //Задание №1
+    int number;
+    scanf("%d", &number);
+
+    printf("%d/n", summa(number));
+
+    //Задание №2
+    int lineSize;
+    scanf("%d", &lineSize);
+    char line[lineSize];
+    for (int i = 0; i <= lineSize; ++i) {
+        scanf("%c", &line[i]);
+    }
+
+    space(line, 0, lineSize);
+
+    printf("%s", line);
+    return 0;
+}
